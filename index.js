7088418d6d427b1ee663979092f99fb2cdc83277
Ana Laura de Araujo <anaraujo0847@gmail.com>
2020-10-17 11:17:49 -0300

    Including watchify, jquery and express-validator

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 const bodyParser = require('body-parser');
-
-const mainTemplate = require('./views/template.js');
 
 const app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.set( 'view engine', 'ejs');
+
 app.get('/', (req, res) => {
-	res.send(mainTemplate());
+	res.render('template');
 });
 
-app.post('/', (req, res) => {
-    const { email, password, passwordConfirmation } = req.body;
-	res.send('Account created!');
+app.post('/', [
+    check('email')
+        .trim()
+        .isEmail()
+        .normalizeEmail(),
+    check('cpf'),
+    check('data_nasc'),
+    check('senha').trim().isLength({ min: 6, max: 20 }),    
+],(req, res) => {
+    const errors = validationResult(req);
+    console.log(errors);
+
+    const { email, cpf, data_nasc, senha } = req.body;
+	res.send(mainTemplate());
 });
 
 app.listen(3000, () => {
