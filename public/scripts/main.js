df8509ff2781c9440904ab5fe526176ea96ae14e
Ana Laura de Araujo <anaraujo0847@gmail.com>
2020-10-19 14:45:46 -0300

    Fixing input style when field is filled

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,16 @@
 const $ = require( 'jquery' );
 const JqueryMaskPlugin = require('jquery-mask-plugin');
 
+function checkFilledInput(el) {
+    input = $(el);
+
+    if (input.val().length > 0) {
+        input.addClass('active');
+      } else {
+        input.removeClass('active');
+      }
+}
+
 $(function() {
     $('#cpf_input').mask('000.000.000-00', {reverse: true});
     $('#birth_input').mask('00/00/0000');
@@ -10,7 +20,14 @@ $(function() {
     setTimeout(function(){
         $('.success-msg').removeClass('show');
     }, 3000);
-    
+
+    $('input').each(function() {
+        checkFilledInput(this);
+    });
+
+    $('input').on('change', function() {
+        checkFilledInput(this);
+    })
 
     $('#password_toggle').on('click', function() {
         let passwordWrapper = $('.password-wrapper');
